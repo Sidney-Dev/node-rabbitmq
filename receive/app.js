44e12bb6caff12215c714d547e6975b748668686
SidneySousa <sidney.sousa@mtn.com>
2023-06-27 13:14:41 +0200

    send and receive via RabbitMQ

---
+++ receive/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4002;
+
+const amqp = require("amqplib");
+var channel, connection;
+
+
+connectQueue() // call connectQueue function
+
+app.listen(PORT, () => console.log("Server running at port " + PORT));
+
+async function connectQueue() {
+    try {
+
+        connection = await amqp.connect(process.env.RABBITMQ_URL);
+        channel = await connection.createChannel()
+        
+        // connect to 'test-queue', create one if doesnot exist already
+        await channel.assertQueue("cms-logs")
+        
+        channel.consume("cms-logs", data => {
+            console.log(JSON.parse(data.content, true))
+            channel.ack(data)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ send/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const amqp = require("amqplib");
+
+const app = express();
+const PORT = 4001;
+
+var channel, connection;  //global variables
+
+app.use(express.json());
+
+
+app.get("/send-msg", (req, res) => {
+    
+    // data to be sent
+    const data = {
+        title  : "walk the dogs",
+        author : "Sidney de Sousa"
+    }
+    sendData(data);  // pass the data to the function we defined
+    console.log("A message is sent to queue")
+    res.send("Message Sent"); //response to the API request
+    
+})
+
+app.listen(PORT, () => {
+    console.log("Server running at port " + PORT); 
+    connectQueue(); 
+});
+
+async function connectQueue() {   
+    try {
+        connection = await amqp.connect(process.env.RABBITMQ_URL);
+        channel    = await connection.createChannel()
+        
+        const queue = await channel.assertQueue("cms-logs")
+        console.log(queue)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function sendData (data) {
+    await channel.sendToQueue("test-queue", Buffer.from(JSON.stringify(data)));
+    await channel.close();
+    await connection.close(); 
+}
